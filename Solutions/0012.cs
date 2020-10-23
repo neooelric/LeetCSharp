06f9750117318f60439d2435cb37cdbb0aa3e5a8
neooelric <neooelric@outlook.com>
2020-10-23 17:47:36 +0800

    0012

---
+++ Solutions/0012.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solutions._0012
+{
+    /*
+    the trick is to treat 4/9/40/90/400/900 as a single symbol
+    */
+
+    public class Solution
+    {
+        public string IntToRoman(int num)
+        {
+            SortedDictionary<int, string> romanSymbolOfNumber = new SortedDictionary<int, string>
+            {
+                {1, "I" },
+                {4,"IV" },
+                {5,"V" },
+                {9,"IX" },
+                {10,"X" },
+                {40,"XL" },
+                {50,"L" },
+                {90,"XC" },
+                {100,"C" },
+                {400,"CD" },
+                {500,"D" },
+                {900,"CM" },
+                {1000,"M" }
+            };
+            StringBuilder sb = new StringBuilder();
+
+            while(num > 0)
+            {
+                foreach(KeyValuePair<int,string> kvp in romanSymbolOfNumber.Reverse())
+                {
+                    if(num >= kvp.Key)
+                    {
+                        num -= kvp.Key;
+                        sb.Append(kvp.Value);
+                        break;
+                    }
+                }
+
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Tests/0012.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Tests._0012
+{
+    public class TestCase
+    {
+        public int Num { get; set; }
+        public string Output { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Num:{0}, Output:{1}", Num, Output);
+        }
+
+        public static List<TestCase> ParseTestCaseFromTextFile(string filePath)
+        {
+            string[] fileContent = System.IO.File.ReadAllLines(filePath);
+
+            List<TestCase> cases = new List<TestCase>();
+
+            for (int i = 0; i < fileContent.Length; ++i)
+            {
+                string line = fileContent[i];
+
+                switch(i % 3)
+                {
+                    case 0:
+                        cases.Add(new TestCase());
+                        cases.Last().Num = int.Parse(line);
+                        break;
+                    case 1:
+                        cases.Last().Output = line;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return cases;
+        }
+    }
+
+    public class Test
+    {
+        [Fact]
+        public void RunTestCases()
+        {
+            Solutions._0012.Solution solution = new Solutions._0012.Solution();
+
+            List<TestCase> cases = TestCase.ParseTestCaseFromTextFile(@"./0012.txt");
+
+            foreach (TestCase c in cases)
+            {
+                string result = solution.IntToRoman(c.Num);
+
+                Assert.True(
+                    string.Compare(result, c.Output) == 0,
+                    string.Format("Case{{{0}}}, Result:{{{1}}}", c, result)
+                );
+            }
+        }
+
+    }
+}
