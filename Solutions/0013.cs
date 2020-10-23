5f1babcb491e87c2d48449212028e3831d19a488
neooelric <neooelric@outlook.com>
2020-10-23 17:47:43 +0800

    0013

---
+++ Solutions/0013.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solutions._0013
+{
+    /*
+    the trick is to treat 4/9/40/90/400/900 as a single symbol
+    */
+
+    public class Solution
+    {
+        public int RomanToInt(string s)
+        {
+            Dictionary<string, int> valueOfRomanSymbol = new Dictionary<string, int>
+            {
+                {"I", 1},
+                {"IV", 4},
+                {"V", 5},
+                {"IX", 9},
+                {"X", 10},
+                {"XL", 40},
+                {"L", 50},
+                {"XC", 90},
+                {"C", 100},
+                {"CD", 400},
+                {"D", 500},
+                {"CM", 900},
+                {"M", 1000}
+            };
+
+            int res = 0;
+            for (int i = 0; i < s.Length; ++i)
+            {
+                string oneCharSymbol = "" + s[i];
+                string twoCharSymbol = i + 1 < s.Length ? "" + s[i] + s[i + 1] : oneCharSymbol;
+                if (valueOfRomanSymbol.ContainsKey(twoCharSymbol))
+                {
+                    res += valueOfRomanSymbol[twoCharSymbol];
+                    ++i;
+                }
+                else if (valueOfRomanSymbol.ContainsKey(oneCharSymbol))
+                {
+                    res += valueOfRomanSymbol[oneCharSymbol];
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Tests/0013.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Tests._0013
+{
+    public class TestCase
+    {
+        public string S { get; set; }
+        public int Output { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("S:\"{0}\", Output:{1}", S, Output);
+        }
+
+        public static List<TestCase> ParseTestCaseFromTextFile(string filePath)
+        {
+            string[] fileContent = System.IO.File.ReadAllLines(filePath);
+
+            List<TestCase> cases = new List<TestCase>();
+
+            for (int i = 0; i < fileContent.Length; ++i)
+            {
+                string line = fileContent[i];
+
+                switch(i % 3)
+                {
+                    case 0:
+                        cases.Add(new TestCase());
+                        cases.Last().S = line;
+                        break;
+                    case 1:
+                        cases.Last().Output = int.Parse(line);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return cases;
+        }
+    }
+
+    public class Test
+    {
+        [Fact]
+        public void RunTestCases()
+        {
+            Solutions._0013.Solution solution = new Solutions._0013.Solution();
+
+            List<TestCase> cases = TestCase.ParseTestCaseFromTextFile(@"./0013.txt");
+
+            foreach (TestCase c in cases)
+            {
+                int result = solution.RomanToInt(c.S);
+
+                Assert.True(
+                    result == c.Output,
+                    string.Format("Case{{{0}}}, Result:{{{1}}}", c, result)
+                );
+            }
+        }
+
+    }
+}
