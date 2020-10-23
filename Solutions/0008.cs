24c00ec7015d5d1866e464f0c13f1ff20ff319b5
neooelric <neooelric@outlook.com>
2020-10-23 14:51:29 +0800

    0008

---
+++ Solutions/0008.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solutions._0008
+{
+    /*
+    pretty simple, just pay attention at overflows
+    */
+
+    public class Solution
+    {
+        public int MyAtoi(string s)
+        {
+            if(string.IsNullOrWhiteSpace(s))
+            {
+                return 0;
+            }
+
+            int res = 0;
+            bool isNegative = false;
+            int i = 0;
+
+            while(char.IsWhiteSpace(s[i]))
+            {
+                i++;
+            }
+
+            if(s[i] == '-' || s[i] == '+')
+            {
+                isNegative = s[i] == '-';
+                i++;
+            }
+
+            while(i < s.Length && char.IsDigit(s[i]))
+            {
+                int digit = s[i] - '0';
+
+                if(isNegative && res < (Int32.MinValue + digit) / 10)
+                {
+                    return Int32.MinValue;
+                }
+
+                if(!isNegative && res > (Int32.MaxValue - digit) / 10)
+                {
+                    return Int32.MaxValue;
+                }
+
+                res *= 10;
+
+                res = isNegative ? (res - digit) : (res + digit);
+
+                i++;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Tests/0008.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Tests._0008
+{
+    public class TestCase
+    {
+        public string S { get; set; }
+        public int Output { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("S:{0}, Output:{1}", S, Output);
+        }
+
+        public static List<TestCase> ParseTestCaseFromTextFile(string filePath)
+        {
+            string[] fileContent = System.IO.File.ReadAllLines(filePath);
+
+            List<TestCase> cases = new List<TestCase>();
+
+            for (int i = 0; i < fileContent.Length; ++i)
+            {
+                string line = fileContent[i];
+
+                switch(i % 3)
+                {
+                    case 0:
+                        cases.Add(new TestCase());
+                        cases.Last().S = line;
+                        break;
+
+                    case 1:
+                        cases.Last().Output = int.Parse(line);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            return cases;
+        }
+    }
+
+    public class Test
+    {
+        [Fact]
+        public void RunTestCases()
+        {
+            Solutions._0008.Solution solution = new Solutions._0008.Solution();
+
+            List<TestCase> cases = TestCase.ParseTestCaseFromTextFile(@"./0008.txt");
+
+            foreach (TestCase c in cases)
+            {
+                int result = solution.MyAtoi(c.S);
+
+                Assert.True(
+                    c.Output == result,
+                    string.Format("Case{{{0}}}, Result:{{{1}}}", c, result)
+                );
+            }
+        }
+
+    }
+}
