7abc7b53a4a789426ec372382c5078b323730f12
neooelric <neooelric@outlook.com>
2020-10-29 11:05:47 +0800

    0028

---
+++ Solutions/0028.cs
@@ -0,0 +1,85 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Solutions._0028
+{
+    /*
+    aha, the classic string match problem
+
+    for this kind of problem, you really should use KMP algorithm to solve
+
+    KMP is not easy to understand, it's a little bit twist, 
+    but it's so classic and widely known, 
+    actually, it becomes the "standard interview answer" to string matching problem
+
+    */
+
+    public class Solution
+    {
+        private int[] getNextArray(string needle)
+        {
+            int[] next = new int[needle.Length];
+            next[0] = -1;
+
+            int i = 0;
+            int j = -1;
+
+            while(i < needle.Length - 1)
+            {
+                if(j == -1 || needle[i] == needle[j])
+                {
+                    i++;
+                    j++;
+                    next[i] = j;
+                }
+                else
+                {
+                    j = next[j];
+                }
+            }
+
+            return next;
+        }
+
+        private int KMP(string haystack, string needle, int[] next)
+        {
+            int i = 0;
+            int j = 0;
+            while(i < haystack.Length && j < needle.Length)
+            {
+                if(j == -1 || haystack[i] == needle[j])
+                {
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    j = next[j];
+                }
+            }
+
+            if(j == needle.Length)
+            {
+                return i - j;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public int StrStr(string haystack, string needle)
+        {
+            if(string.IsNullOrEmpty(needle))
+            {
+                return 0;
+            }
+
+            return KMP(haystack, needle, getNextArray(needle));
+        }
+    }
+}
---
+++ Tests/0028.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Utilities;
+using Xunit;
+
+namespace Tests._0028
+{
+    public class TestCase
+    {
+        public string Haystack { get; set; }
+        public string Needle { get; set; }
+        public int Output { get; set; }
+
+
+        public override string ToString()
+        {
+            return string.Format("Haystack:{0}, Needle:{1}, Output:{2}", Haystack, Needle, Output);
+        }
+
+        public static List<TestCase> ParseTestCaseFromTextFile(string filePath)
+        {
+            string[] fileContent = System.IO.File.ReadAllLines(filePath);
+
+            List<TestCase> cases = new List<TestCase>();
+
+            for (int i = 0; i < fileContent.Length; ++i)
+            {
+                string line = fileContent[i];
+
+                switch (i % 4)
+                {
+                    case 0:
+                        cases.Add(new TestCase());
+                        cases.Last().Haystack = Helper.ParseString(line);
+                        break;
+                    case 1:
+                        cases.Last().Needle = Helper.ParseString(line);
+                        break;
+                    case 2:
+                        cases.Last().Output = Helper.ParseInt(line);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return cases;
+        }
+    }
+
+    public class Test
+    {
+        [Fact]
+        public void RunTestCases()
+        {
+            Solutions._0028.Solution solution = new Solutions._0028.Solution();
+
+            List<TestCase> cases = TestCase.ParseTestCaseFromTextFile(@"./0028.txt");
+
+            foreach (TestCase c in cases)
+            {
+                var result = solution.StrStr(c.Haystack, c.Needle);
+
+                Assert.True(
+                    Helper.Equals(result, c.Output),
+                    string.Format("Case{{{0}}}, Result:{{{1}}}", c, result)
+                    );
+            }
+        }
+
+    }
+}
