352028b3be229ac96a7fcd466a5fb22c1ec4fe25
neooelric <neooelric@outlook.com>
2020-10-30 14:42:56 +0800

    0030

---
+++ Solutions/0030.cs
@@ -0,0 +1,105 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solutions._0030
+{
+    /*
+    dont get fooled, this actually not a string matching problem, you dont need get algorithms like KMP to solve this problem.
+
+    the possible answer is in range [0, s.Length - singleWordLength * wordsCount]
+
+    so the most intuitive thought is to traverse number in range [0, s.Length - singleWordLength * wordsCount]
+    test each of the number is legal or not
+
+    so the main shape of solution is like:
+        for(int i = 0; i <= s.Length - singleWordLength * wordsCount; ++i) {
+            if(i is a valid answer) {
+                res.Add(i);
+            }
+        }
+
+    then, in each loop iteration, the thing you need to judge is actually: 
+        s.subString(i, i + singleWordLength * wordsCount) whether contains all words and each word only once
+    in another word, is to:
+        just to judge string array
+            [
+                s.subString(i, i + singleWordLength),
+                s.subString(i+singleWordLength, i+2*singleWordLength),
+                ...
+            ]
+        and string array "words" is, or is not equals, regardless of order
+    */
+
+    public class Solution
+    {
+        private bool IsSubstringContainsAllWords(
+            string s,
+            int beginIndex,
+            Dictionary<string, int> wordCountDict,
+            int singleWordLength,
+            int wordsCount)
+        {
+            Dictionary<string, int> wordCountInHere = new Dictionary<string, int>();
+
+            for(int i = 0; i < wordsCount; ++i)
+            {
+                string word = s.Substring(beginIndex + i * singleWordLength, singleWordLength);
+                if(!wordCountInHere.ContainsKey(word))
+                {
+                    wordCountInHere.Add(word, 1);
+                }
+                else
+                {
+                    wordCountInHere[word]++;
+                }
+
+                if(!wordCountDict.ContainsKey(word) || wordCountDict[word] < wordCountInHere[word])
+                {
+                    return false;
+                }
+
+            }
+
+            return true;
+
+        }
+
+        public IList<int> FindSubstring(string s, string[] words)
+        {
+            List<int> res = new List<int>();
+
+            if(words.Length == 0)
+            {
+                return res;
+            }
+
+            Dictionary<string, int> wordCountDict = new Dictionary<string, int>();
+            foreach(string word in words)
+            {
+                if(!wordCountDict.ContainsKey(word))
+                {
+                    wordCountDict.Add(word, 0);
+                }
+                wordCountDict[word]++;
+            }
+
+
+            int singleWordLength = words[0].Length;
+            int wordsCount = words.Length;
+
+            for(int i = 0; i <= s.Length - singleWordLength * wordsCount; ++i)
+            {
+                if(IsSubstringContainsAllWords(s, i, wordCountDict, singleWordLength, wordsCount))
+                {
+                    res.Add(i);
+                }
+            }
+
+
+            return res;
+        }
+    }
+}
---
+++ Tests/0030.cs
@@ -0,0 +1,74 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Utilities;
+using Xunit;
+
+namespace Tests._0030
+{
+    public class TestCase
+    {
+        public string S { get; set; }
+        public string[] Words { get; set; }
+        public int[] Output { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("S:{0}, Words:{1}, Output:{2}", S, Helper.FormatStringArray(Words), Helper.FormatIntArray(Output));
+        }
+
+        public static List<TestCase> ParseTestCaseFromTextFile(string filePath)
+        {
+            string[] fileContent = System.IO.File.ReadAllLines(filePath);
+
+            List<TestCase> cases = new List<TestCase>();
+
+            for (int i = 0; i < fileContent.Length; ++i)
+            {
+                string line = fileContent[i];
+
+                switch (i % 4)
+                {
+                    case 0:
+                        cases.Add(new TestCase());
+                        cases.Last().S = Helper.ParseString(line);
+                        break;
+                    case 1:
+                        cases.Last().Words = Helper.ParseStringArray(line);
+                        break;
+                    case 2:
+                        cases.Last().Output = Helper.ParseIntArray(line);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return cases;
+        }
+    }
+
+    public class Test
+    {
+        [Fact]
+        public void RunTestCases()
+        {
+            Solutions._0030.Solution solution = new Solutions._0030.Solution();
+
+            List<TestCase> cases = TestCase.ParseTestCaseFromTextFile(@"./0030.txt");
+
+            foreach (TestCase c in cases)
+            {
+                var result = solution.FindSubstring(c.S, c.Words);
+
+                Assert.True(
+                    Helper.Equals(result.ToArray(), c.Output),
+                    string.Format("Case{{{0}}}, Result:{{{1}}}", c, Helper.FormatIntArray(result.ToArray()))
+                    );
+            }
+        }
+
+    }
+}
