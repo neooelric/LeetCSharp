a5579aa25019f9780ffe6490cff96064b0baff98
neooelric <neooelric@outlook.com>
2020-10-30 10:49:17 +0800

    0029

---
+++ Solutions/0029.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+
+namespace Solutions._0029
+{
+    /*
+    it is very obviously that this problem is ask you to do "divide" operation manually
+    using code logic to simiulate the "divide" logic
+
+    the basic train of thought is :
+        step 1 : check the answer should be positive or negative, 
+                 then convert two operand both to it's Abs value, maybe store it into a Unsigned type variable
+        step 2 : using minus in a loop, before dividend goes less than divisor, the minus count is the answer
+
+    so there are two key points:
+        1. range of int(Int32) is : [-0x80000000, 0x7fffffff]
+        2. minus in loop is pretty low performance, we have to find a way to optimize this, speed the loop
+           the most intuitive way is to using bit operation to speed up the loop
+    */
+
+    public class Solution
+    {
+        public UInt32 IntAbsToUInt32(int num)
+        {
+            if(num > 0)
+            {
+                return Convert.ToUInt32(num);
+            }
+
+            if(num == int.MinValue)
+            {
+                return 0x80000000;
+            }
+
+            return Convert.ToUInt32(-num);
+        }
+        public int Divide(int dividend, int divisor)
+        {
+            // step 1. convert operand to UInt32 and handle some corner cases
+            bool resIsNegative = (dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0);
+
+            UInt32 unsignedDividend = IntAbsToUInt32(dividend);
+            UInt32 unsignedDivisor = IntAbsToUInt32(divisor);
+
+            // step 2 : simulate divide operation
+            UInt32 res = 0;
+            while(unsignedDividend >= unsignedDivisor)
+            {
+                UInt32 multiplesCountOfDivisor = 1;
+                UInt32 minuend = unsignedDivisor;
+
+                while(minuend < unsignedDividend)
+                {
+                    minuend <<= 1;
+                    multiplesCountOfDivisor <<= 1;
+                }
+
+                if (minuend > unsignedDividend)
+                {
+                    minuend >>= 1;
+                    multiplesCountOfDivisor >>= 1;
+                }
+
+                unsignedDividend -= minuend;
+                res += multiplesCountOfDivisor;
+            }
+
+            if(res > 0x80000000)
+            {
+                return int.MaxValue;
+            }
+
+            if(res == 0x80000000)
+            {
+                return resIsNegative ? int.MinValue : int.MaxValue;
+            }
+
+            return resIsNegative ? -Convert.ToInt32(res) : Convert.ToInt32(res);
+        }
+
+    }
+}
---
+++ Tests/0029.cs
@@ -0,0 +1,74 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Utilities;
+using Xunit;
+
+namespace Tests._0029
+{
+    public class TestCase
+    {
+        public int Dividend { get; set; }
+        public int Divisor { get; set; }
+        public int Output { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Dividend:{0}, Divisor:{1}, Output:{2}", Dividend, Divisor, Output);
+        }
+
+        public static List<TestCase> ParseTestCaseFromTextFile(string filePath)
+        {
+            string[] fileContent = System.IO.File.ReadAllLines(filePath);
+
+            List<TestCase> cases = new List<TestCase>();
+
+            for (int i = 0; i < fileContent.Length; ++i)
+            {
+                string line = fileContent[i];
+
+                switch (i % 4)
+                {
+                    case 0:
+                        cases.Add(new TestCase());
+                        cases.Last().Dividend = Helper.ParseInt(line);
+                        break;
+                    case 1:
+                        cases.Last().Divisor = Helper.ParseInt(line);
+                        break;
+                    case 2:
+                        cases.Last().Output = Helper.ParseInt(line);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return cases;
+        }
+    }
+
+    public class Test
+    {
+        [Fact]
+        public void RunTestCases()
+        {
+            Solutions._0029.Solution solution = new Solutions._0029.Solution();
+
+            List<TestCase> cases = TestCase.ParseTestCaseFromTextFile(@"./0029.txt");
+
+            foreach (TestCase c in cases)
+            {
+                var result = solution.Divide(c.Dividend, c.Divisor);
+
+                Assert.True(
+                    Helper.Equals(result, c.Output),
+                    string.Format("Case{{{0}}}, Result:{{{1}}}", c, result)
+                    );
+            }
+        }
+
+    }
+}
