b3abdef4897742f70dfa3ccc275b7848d102b66e
neooelric <neooelric@outlook.com>
2020-11-02 11:24:11 +0800

    0031

---
+++ Solutions/0031.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solutions._0031
+{
+    /*
+    There is a classic algorithm of how to generate next permutation in lexicographic ordering, and it can handle repeated values.
+    This algorithm goes back to Narayana Pandita in 14th century India.
+
+        1. Find the largest index "k" such that "a[k] < a [k+1]", if no such index exists, the permutation is the last permutation
+        2. Find the largest index "l" greater than "k" such that "a[k] < a[l]"
+        3. Swap the value of "a[k]" with that of "a[l]"
+        4. Reverse the sequence from "a[k+1]" up to and including the final element "a[n]"
+    */
+
+    public class Solution
+    {
+        private void Reverse(int[] nums, int startIndex, int endIndex)
+        {
+            for(int left=startIndex, right=endIndex; left < right; left++, right--)
+            {
+                int tmp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = tmp;
+            }
+        }
+
+        public void NextPermutation(int[] nums)
+        {
+            if(nums.Length < 2)
+            {
+                return;
+            }
+
+            int k = nums.Length - 2;
+            while(k >= 0 && nums[k] >= nums[k+1])
+            {
+                k--;
+            }
+
+            if(k < 0)
+            {
+                Reverse(nums, 0, nums.Length - 1);
+                return;
+            }
+
+            int l = nums.Length - 1;
+            while(l > k && nums[l] <= nums[k])
+            {
+                l--;
+            }
+
+            int tmp = nums[k];
+            nums[k] = nums[l];
+            nums[l] = tmp;
+
+            Reverse(nums, k + 1, nums.Length - 1);
+        }
+
+    }
+}
---
+++ Tests/0031.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Utilities;
+using Xunit;
+
+namespace Tests._0031
+{
+    public class TestCase
+    {
+        public int[] Nums { get; set; }
+        public int[] Output { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Nums:{0}, Output:{1}", Helper.FormatIntArray(Nums), Helper.FormatIntArray(Output));
+        }
+
+        public static List<TestCase> ParseTestCaseFromTextFile(string filePath)
+        {
+            string[] fileContent = System.IO.File.ReadAllLines(filePath);
+
+            List<TestCase> cases = new List<TestCase>();
+
+            for (int i = 0; i < fileContent.Length; ++i)
+            {
+                string line = fileContent[i];
+
+                switch (i % 3)
+                {
+                    case 0:
+                        cases.Add(new TestCase());
+                        cases.Last().Nums = Helper.ParseIntArray(line);
+                        break;
+                    case 1:
+                        cases.Last().Output = Helper.ParseIntArray(line);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return cases;
+        }
+    }
+
+    public class Test
+    {
+        [Fact]
+        public void RunTestCases()
+        {
+            Solutions._0031.Solution solution = new Solutions._0031.Solution();
+
+            List<TestCase> cases = TestCase.ParseTestCaseFromTextFile(@"./0031.txt");
+
+            foreach (TestCase c in cases)
+            {
+                int[] result = Helper.DupIntArray(c.Nums);
+                solution.NextPermutation(result);
+
+                Assert.True(
+                    Helper.Equals(result, c.Output),
+                    string.Format("Case{{{0}}}, Result:{{{1}}}", c, Helper.FormatIntArray(result))
+                    );
+            }
+        }
+
+    }
+}
