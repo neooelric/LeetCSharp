e520c6f31c320149ea270ddafc81fbdb7b8a7f9f
neooelric <neooelric@outlook.com>
2020-11-03 17:31:41 +0800

    0035

---
+++ Solutions/0035.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solutions._0035
+{
+    /*
+    simple binary-search problem.
+    */
+
+    public class Solution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while(left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if(nums[mid] == target)
+                {
+                    return mid;
+                }
+
+                if(nums[mid] > target)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return left;
+        }
+
+    }
+}
---
+++ Tests/0035.cs
@@ -0,0 +1,74 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Utilities;
+using Xunit;
+
+namespace Tests._0035
+{
+    public class TestCase
+    {
+        public int[] Nums { get; set; }
+        public int Target { get; set; }
+        public int Output { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Nums:{0}, Target:{1}, Output:{2}", Helper.FormatIntArray(Nums), Target, Output);
+        }
+
+        public static List<TestCase> ParseTestCaseFromTextFile(string filePath)
+        {
+            string[] fileContent = System.IO.File.ReadAllLines(filePath);
+
+            List<TestCase> cases = new List<TestCase>();
+
+            for (int i = 0; i < fileContent.Length; ++i)
+            {
+                string line = fileContent[i];
+
+                switch (i % 4)
+                {
+                    case 0:
+                        cases.Add(new TestCase());
+                        cases.Last().Nums = Helper.ParseIntArray(line);
+                        break;
+                    case 1:
+                        cases.Last().Target = Helper.ParseInt(line);
+                        break;
+                    case 2:
+                        cases.Last().Output = Helper.ParseInt(line);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return cases;
+        }
+    }
+
+    public class Test
+    {
+        [Fact]
+        public void RunTestCases()
+        {
+            Solutions._0035.Solution solution = new Solutions._0035.Solution();
+
+            List<TestCase> cases = TestCase.ParseTestCaseFromTextFile(@"./0035.txt");
+
+            foreach (TestCase c in cases)
+            {
+                var result = solution.SearchInsert(c.Nums, c.Target);
+
+                Assert.True(
+                    Helper.Equals(result, c.Output),
+                    string.Format("Case{{{0}}}, Result:{{{1}}}", c, result)
+                    );
+            }
+        }
+
+    }
+}
